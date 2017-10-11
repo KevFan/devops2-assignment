23a845b2b3732b95dd1908761591eba7fc817821
KevFan <kevin_fan@hotmail.co.uk>
2017-10-11 20:44:42 +0100

    Main, login, signup views + routes + update to later version of semantic ui

--- app/controllers/tweets.js
+++ app/controllers/tweets.js
@@ -7,3 +7,19 @@ exports.home = {
   },
 
 };
+
+exports.signup = {
+
+  handler: (request, reply) => {
+    reply.view('signup', { title: 'MyTweet | Sign Up' });
+  },
+
+};
+
+exports.login = {
+
+  handler: (request, reply) => {
+    reply.view('login', { title: 'MyTweet | Login' });
+  },
+
+};
--- routes.js
+++ routes.js
@@ -4,6 +4,8 @@ const Assets = require('./app/controllers/assets');
 module.exports = [
 
   { method: 'GET', path: '/', config: Tweets.home },
+  { method: 'GET', path: '/signup', config: Tweets.signup },
+  { method: 'GET', path: '/login', config: Tweets.login },
 
   {
     method: 'GET',
