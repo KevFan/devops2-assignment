1e09c402b339eded2678dd694bf40d2d0d9c9368
KevFan <kevin_fan@hotmail.co.uk>
2017-10-19 19:42:01 +0100

    List all users in admin dashboard

--- app/controllers/master.js
+++ app/controllers/master.js
@@ -1,11 +1,16 @@
 const Admin = require('../models/admin');
+const User = require('../models/user');
 
 exports.home = {
 
   handler: function (request, reply) {
     const userId = request.auth.credentials.loggedInUser;
+    let admin = null;
     Admin.findOne({ _id: userId }).then(foundAdmin => {
-      reply.view('adminDashboard', { title: 'Admin Dashboard', admin: foundAdmin });
+      admin = foundAdmin;
+      return User.find({});
+    }).then(allUsers => {
+      reply.view('adminDashboard', { title: 'Admin Dashboard', admin: admin, user: allUsers });
     }).catch(err => {
       reply.redirect('/');
     });
