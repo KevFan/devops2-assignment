c2f68524ef414a5e28b04b6fd912a2bd22ec7d64
KevFan <kevin_fan@hotmail.co.uk>
2017-10-31 12:36:59 +0000

    Added boom + usersapi + routesapi

---
+++ app/api/usersapi.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const User = require('../models/user');
+const Boom = require('boom');
+
+exports.find = {
+
+  auth: false,
+
+  handler: function (request, reply) {
+    User.find({}).exec().then(users => {
+      reply(users);
+    }).catch(err => {
+      reply(Boom.badImplementation('error accessing db'));
+    });
+  },
+
+};
+
+exports.findOne = {
+
+  auth: false,
+
+  handler: function (request, reply) {
+    User.findOne({ _id: request.params.id }).then(user => {
+      reply(user);
+    }).catch(err => {
+      reply(Boom.notFound('id not found'));
+    });
+  },
+
+};
+
+exports.create = {
+
+  auth: false,
+
+  handler: function (request, reply) {
+    const user = new User(request.payload);
+    user.save().then(newUser => {
+      reply(newUser).code(201);
+    }).catch(err => {
+      reply(Boom.badImplementation('error creating user'));
+    });
+  },
+
+};
+
+exports.deleteAll = {
+
+  auth: false,
+
+  handler: function (request, reply) {
+    User.remove({}).then(err => {
+      reply().code(204);
+    }).catch(err => {
+      reply(Boom.badImplementation('error removing users'));
+    });
+  },
+
+};
+
+exports.deleteOne = {
+
+  auth: false,
+
+  handler: function (request, reply) {
+    User.remove({ _id: request.params.id }).then(user => {
+      reply(user).code(204);
+    }).catch(err => {
+      reply(Boom.notFound('id not found'));
+    });
+  },
+
+};
+
+exports.update = {
+  auth: false,
+
+  handler: function (request, reply) {
+    User.findOneAndUpdate({ _id: request.params.id }, request.payload, { new: true }).then(user => {
+      reply(user).code(200);
+    }).catch(err => {
+      reply(Boom.notFound('error updating user'));
+    });
+  },
+};
